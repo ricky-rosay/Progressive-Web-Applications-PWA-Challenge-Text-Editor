37d159516abb46ed041c9cc85a66b761958c2441
ricky-rosay <111669764+ricky-rosay@users.noreply.github.com>
2023-02-05 17:25:59 -0500

    logic that accepts content

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,7 +13,16 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// this will take some content and add it to the index db with the ibd module
+export const putDb = async (content) => {
+  console.log('give it to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('the data has been saved to the DB', result.value);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
